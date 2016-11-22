328d710dc6e9fc25132a532b8389061e2bc17ec1
seungshins <seungshins@gmail.com>
2016-11-22 05:00:47 +0000

    change todo sample app

--- sampleApp/src/routes/todo/Todo.js
+++ sampleApp/src/routes/todo/Todo.js
@@ -13,48 +13,120 @@ import Layout from '../../components/Layout';
 import s from './Todo.css';
 import BootStrap from 'react-bootstrap';
 
-// function Todo({ title }) {
-//   return (
-//     <Layout>
-//       <div className={s.root}>
-//         <div className={s.container}>
-//           <App11></App11>
-//         </div>
-//       </div>
-//     </Layout>
-//   );
-// }
+function Todo({ title }) {
+  return (
+    <Layout>
+      <div className={s.root}>
+        <div className={s.container}>
+          <App11></App11>
+        </div>
+      </div>
+    </Layout>
+  );
+}
 
-var serverData = [
-  { id: 1, name: 'kss' },
-  { id: 2, name: 'kss1' },
-  { id: 3, name: 'kss2' },
+// var serverData = [
+//   { id: 1, name: 'kss' },
+//   { id: 2, name: 'kss1' },
+//   { id: 3, name: 'kss2' },
+// ];
+
+// var Todo = React.createClass({
+//   getInitialState() {
+//     return {
+//       message: 'loading...',
+//       data: serverData
+//     };
+//   },
+
+//   componentWillMount() {
+//     this.setState({message: 'welcome!', data: serverData});
+//   },
+
+//   render() {
+//     if(this.state.data) {
+//       var list = this.state.data.map(obj => <li key={obj.id}>{obj.id}:{obj.name}</li>);
+//     }
+//     return (
+//       <div>
+//         <p>server-side rendering sample</p>
+//         <p>{this.state.message}</p>
+//         <ul>{list}</ul>
+//       </div>
+//     );
+//   }
+// });
+
+var services = [
+    { name: 'JAVA', price: 80 },
+    { name: 'SQL', price: 33 },
+    { name: 'SERVLET', price: 66 },
+    { name: 'JSP', price: 99 }
 ];
 
-var Todo = React.createClass({
-  getInitialState() {
-    return {
-      message: 'loading...',
-      data: serverData
-    };
-  },
+var App11 = React.createClass({
 
-  componentWillMount() {
-    this.setState({message: 'welcome!', data: serverData});
-  },
+    getInitialState: function(){
+        return { total: 0, items: services };
+    },
 
-  render() {
-    if(this.state.data) {
-      var list = this.state.data.map(obj => <li key={obj.id}>{obj.id}:{obj.name}</li>);
+    addTotal: function( price ){
+        this.setState( { total: this.state.total + price } );
+    },
+
+    render: function() {
+
+        var self = this;
+
+        var services = this.state.items.map(function(ser, idx){
+
+            // Create a new Service component for each item in the items array.
+            // Notice that I pass the self.addTotal function to the component.
+
+            return <ServiceLayer name={ser.name} price={ser.price} active={ser.active} addTotal={self.addTotal} key={idx}/>;
+        });
+
+        return <div>
+                    <h1>Our services</h1>
+                    
+                    <div id="services">
+                        {services}
+
+                        <p id="total">Total <b>{this.state.total.toFixed(2)}점</b></p>
+
+                    </div>
+
+                </div>;
+
     }
-    return (
-      <div>
-        <p>server-side rendering sample</p>
-        <p>{this.state.message}</p>
-        <ul>{list}</ul>
-      </div>
-    );
-  }
+});
+
+
+var ServiceLayer = React.createClass({
+
+    getInitialState: function(){
+        return { active: false };
+    },
+
+    clickHandler: function (){
+
+        var active = !this.state.active;
+          console.log("activ",active);
+
+        this.setState({ active: active });
+        
+        // Notify the Todo, by calling its addTotal method
+        this.props.addTotal( active ? this.props.price : -this.props.price );
+
+    },
+
+    render: function(){
+        return  <p className={ this.state.active ? 'active' : '' } onClick={this.clickHandler}>
+                    {this.props.name} <b>{this.props.price.toFixed(2)}점</b>
+                </p>;
+
+    }
+
 });
 
 Todo.propTypes = {
